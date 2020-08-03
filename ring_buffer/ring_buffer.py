58a914e58c06f4bfc8fb159ad017449151156165
Sanjay Krishna <skp68@mst.edu>
2020-08-02 20:17:43 -0700

    mvp

--- ring_buffer/ring_buffer.py
+++ ring_buffer/ring_buffer.py
@@ -1,9 +1,22 @@
 class RingBuffer:
     def __init__(self, capacity):
+        self.items = []
+        self.capacity = capacity
+        self.index = 0
         pass
 
     def append(self, item):
+        if len(self.items) < self.capacity:
+            self.items.append(item)
+
+        else:
+            self.items[self.index] = item
+            self.index += 1
+        
+        if self.index == self.capacity:
+            self.index = 0          
         pass
 
     def get(self):
+        return self.items
         pass
